c2a7f15c72f2509719f209cebc512cb7b0da6c04
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2014-07-15 12:37:56 +0200

    Added option for ordering select results.

--- example.js
+++ example.js
@@ -7,59 +7,14 @@ var options = {
 var virgilio = new Virgilio(options);
 virgilio.loadModule(require('./lib/virgilio-mongo'));
 
-virgilio.execute('mongo.query', {
-    from: 'people',
-    where: [
-        ['nationality', '==', 'dutch']
-    ],
-    select: ['name']
-})
-.then(function(response) {
-    console.log('RESPONSE');
-    console.log(response);
-    process.exit();
-})
-.done();
-
-// virgilio.execute('mongo.query', {
-//     into: 'frameworks',
-//     insert: [
-//         { name: 'virgilio', state: 'awesome' }
-//     ]
-// })
-// .then(function(response) {
-//     console.log('RESPONSE');
-//     console.log(response);
-// })
-// .done();
-
-
-
-// //1
-// virgilio.mongo
-//     .from('people')
-//     .where('nationality', '==', 'italian')
-//     .orderBy('name')
-//     .select('name')
-//     .then(function(italians) {
-//         console.log(italians); //Gabriele, Luca, Valerio
-//     });
-
-// //2
-// virgilio.execute('mongo', {
-//     from: 'people',
-//     where: ['nationality', '==', 'italian'],
-//     select: 'name',
-//     orderBy: 'name'
-// });
-
-// //3
-// var db = {}; //Mongo client created using NPM mongo-db
-// var query = {
-//     fields: ['name'],
-//     sort: ['name']
-// };
-
-// db.collectionAsync('people')
-//     .call('findAsync', query)
-//     .call('toArrayAsync');
+virgilio.mongo()
+    .from('people')
+    .where('nationality', '==', 'dutch')
+    .orderBy('name', 'desc')
+    .select('name')
+    .then(function(response) {
+        console.log('RESPONSE');
+        console.log(response);
+        process.exit();
+    })
+    .done();
